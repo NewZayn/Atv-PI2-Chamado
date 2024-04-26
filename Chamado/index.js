13260039dd3fe52e5fb4312a410be8ad4d679c1d
Gabriel Severino Lopes Guimarães Estacio <118941322+NewZayn02@users.noreply.github.com>
2024-04-26 04:53:14 -0300

    Update index.js

--- Chamado/index.js
+++ Chamado/index.js
@@ -1,9 +1,3 @@
-const tarefaURL = "https://parseapi.back4app.com/classes/Chamado";
-const headers = {
-    "X-Parse-Application-Id": "lknxQw2TygNmmkTwsY02D09lxXlzLklEUEPnuZ1I",
-    "X-Parse-REST-API-Key": "mSwbnPjpwvDwCmX05NASyClqUYRpaK64A6k1aMMM",
-};
-
 const formChamado = document.getElementById("formChamado");
 
 formChamado.addEventListener("submit", async (event) => {
@@ -13,7 +7,7 @@ formChamado.addEventListener("submit", async (event) => {
     const data = Object.fromEntries(formData.entries()); // Converte o FormData para um objeto JavaScript
 
     try {
-        const response = await fetch('https://parseapi.back4app.com', {
+        const response = await fetch('https://parseapi.back4app.com/classes/Chamado', {
             method: 'POST',
             headers: {
                 'X-Parse-Application-Id': 'lknxQw2TygNmmkTwsY02D09lxXlzLklEUEPnuZ1I',
@@ -31,21 +25,5 @@ formChamado.addEventListener("submit", async (event) => {
     } catch (error) {
         console.error('Erro ao enviar chamado:', error.message);
     }
-  
-    // Exemplo de código para exibir os dados do formulário no console
-    formData.forEach(function(value, key) {
-        console.log(key + ': ' + value);
-    });
 });
-// Captura o elemento h1 pelo ID
-const titulo = document.getElementById('titulo');
-
-// Verifica se o elemento foi encontrado
-if (titulo) {
-    // Altera o texto do elemento h1
-    titulo.textContent = 'Conexão com JavaScript bem-sucedida!';
-} else {
-    console.error('Elemento h1 não encontrado!');
-}
-
 
