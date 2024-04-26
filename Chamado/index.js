6fd496aaa98e640b59f1955233f868250667977e
Gabriel Severino Lopes Guimarães Estacio <118941322+NewZayn02@users.noreply.github.com>
2024-04-26 07:12:22 +0000

    test

---
+++ Chamado/index.js
@@ -0,0 +1,36 @@
+
+
+const tarefaURL = "https://parseapi.back4app.com/classes/Tarefa";
+const headers = {
+  "X-Parse-Application-Id": "lknxQw2TygNmmkTwsY02D09lxXlzLklEUEPnuZ1I",
+  "X-Parse-REST-API-Key": "JRe8VT03ds0DvVHbHV3py2hOjY0EUIT3FcuTrnBx",
+};
+
+const formChamado = document.getElementById("formChamado");
+
+formChamado.addEventListener("submit", async (event) => {
+    event.preventDefault(); // Impede o comportamento padrão de envio do formulário
+
+    const formData = new FormData(formChamado);
+    const data = Object.fromEntries(formData.entries()); // Converte o FormData para um objeto JavaScript
+
+    try {
+        const response = await fetch('https://parseapi.back4app.com', {
+            method: 'POST',
+            headers: {
+                'X-Parse-Application-Id': 'lknxQw2TygNmmkTwsY02D09lxXlzLklEUEPnuZ1I',
+                'X-Parse-REST-API-Key': 'JRe8VT03ds0DvVHbHV3py2hOjY0EUIT3FcuTrnBx',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            console.log('Chamado enviado com sucesso!');
+        } else {
+            console.error('Erro ao enviar chamado:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Erro ao enviar chamado:', error.message);
+    }
+});
