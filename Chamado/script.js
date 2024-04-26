c6ddea45dfe8bba2781aff001ceddad395ba6247
Gabriel Severino Lopes Guimarães Estacio <118941322+NewZayn02@users.noreply.github.com>
2024-04-26 08:28:23 +0000

    h

--- Chamado/script.js
+++ Chamado/script.js
@@ -36,6 +36,8 @@ document.addEventListener('DOMContentLoaded', async function() {
             const response = await fetch('https://parseapi.back4app.com/classes/Chamado', {
                 method: 'POST',
                 headers: {
+                    'X-Parse-Application-Id': 'lknxQw2TygNmmkTwsY02D09lxXlzLklEUEPnuZ1I',
+                    'X-Parse-REST-API-Key': 'mSwbnPjpwvDwCmX05NASyClqUYRpaK64A6k1aMMM',
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({ resposta })
