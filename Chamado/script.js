0c3294e25d1be0b60cc6fc0b0298e91607dc5762
NewZayn <97660788+NewZayn@users.noreply.github.com>
2024-04-26 20:00:05 -0300

    Update script.js

--- Chamado/script.js
+++ Chamado/script.js
@@ -72,6 +72,33 @@ document.addEventListener('DOMContentLoaded', async function() {
 }
 
 
+    formChamado.addEventListener("submit", async (event) => {
+
+    const formData = new FormData(formChamado);
+    const data = Object.fromEntries(formData.entries()); 
+
+    try {
+        const response = await fetch('https://parseapi.back4app.com/classes/Chamado', {
+            method: 'POST',
+            headers: {
+                'X-Parse-Application-Id': 'RVSqnwoDE2zJ9QRCxCfrG9szWYcIOlKDGt4gwbUR',
+                'X-Parse-REST-API-Key': 'jQ6avsMD5F4M1o6jZpeuLGRwxCN9WXx748mA3dqs',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            console.log('Chamado enviado com sucesso!');
+        } else {
+            console.error('Erro ao enviar chamado:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Erro ao enviar chamado:', error.message);
+    }
+  });
+
+
     // Event listener para o formulário de resposta
     formResposta.addEventListener('submit', async function(event) {
         event.preventDefault();
